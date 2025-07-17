528f69bbe15c142bfb3ee84c32d72630bbb2bdd9
ahmedaidara <ahmedsaidaidara05@gmail.com>
2025-07-17 20:25:28 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('ansar-almouyassar-v1').then((cache) => {
+    caches.open('ansar-almouyassar-v2').then((cache) => {
       return cache.addAll([
         '/',
         '/index.html',
@@ -17,7 +17,7 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request).catch(() => {
-        if (event.request.url.endsWith('.png') || event.request.url.endsWith('.jpg')) {
+        if (event.request.url.endsWith('.png') || event.request.url.endsWith('.jpg') || event.request.url.endsWith('.mp4')) {
           return caches.match('/assets/images/default-photo.png');
         }
         return new Response('Ressource non trouvée', { status: 404 });
