f915f128d3767cdd9d6c6ff6e7c0bdc216302874
ahmedaidara <ahmedsaidaidara05@gmail.com>
2025-07-17 20:00:12 +0000

    Update chatbotResponses.js

--- chatbotResponses.js
+++ chatbotResponses.js
@@ -5,14 +5,14 @@ const chatbotResponses = [
   { keyword: "événement", response: "Consultez les événements à venir dans la section 'Événements'. Les membres du bureau peuvent en ajouter dans l'Espace Secret." },
   { keyword: "galerie", response: "La galerie contient des photos et vidéos des événements. Accessible à tous dans la section 'Galerie'." },
   { keyword: "espace personnel", response: "Connectez-vous à votre espace personnel avec votre numéro de membre (ex. : 001) et mot de passe (format JJMMAAAA)." },
-  { keyword: "espace secret", response: "L'Espace Secret est réservé aux membres du bureau. Utilisez un code comme ADMIN12301012000 pour y accéder." },
+  { keyword: "espace secret", response: "L'Espace Secret est réservé aux membres du bureau. Utilisez un code comme ADMIN12301012000, puis un mot de passe spécifique (ex. : JESUISMEMBRE66 pour Admin, JESUISTRESORIER444 pour Trésorier, etc.)." },
   { keyword: "coran", response: "La section 'Coran' affiche les 30 Juz'. Utilisez la barre de recherche pour trouver un Juz spécifique." },
   { keyword: "suggestion", response: "Vous pouvez soumettre une suggestion dans votre Espace Personnel. Les membres du bureau peuvent les consulter dans l'Espace Secret." },
   { keyword: "bibliothèque", response: "La bibliothèque contient des livres classés par catégories (Fiqh, Hadith, Langue, etc.). Accessible dans la section 'Bibliothèque'." },
-  { keyword: "ADMIN12301012000", response: "Veuillez entrer un mot de passe (ex. : JESUISMEMBRE66, JESUISTRESORIER444, PRESIDENT000, SECRETAIRE000)." },
-  { keyword: "00000000", response: "Veuillez entrer un mot de passe (ex. : JESUISMEMBRE66, JESUISTRESORIER444, PRESIDENT000, SECRETAIRE000)." },
-  { keyword: "11111111", response: "Veuillez entrer un mot de passe (ex. : JESUISMEMBRE66, JESUISTRESORIER444, PRESIDENT000, SECRETAIRE000)." },
-  { keyword: "22222222", response: "Veuillez entrer un mot de passe (ex. : JESUISMEMBRE66, JESUISTRESORIER444, PRESIDENT000, SECRETAIRE000)." }
+  { keyword: "ADMIN12301012000", response: "Veuillez entrer un mot de passe (ex. : JESUISMEMBRE66 pour Admin, JESUISTRESORIER444 pour Trésorier, PRESIDENT000 pour Président, SECRETAIRE000 pour Secrétaire)." },
+  { keyword: "00000000", response: "Veuillez entrer un mot de passe (ex. : JESUISMEMBRE66 pour Admin, JESUISTRESORIER444 pour Trésorier, PRESIDENT000 pour Président, SECRETAIRE000 pour Secrétaire)." },
+  { keyword: "11111111", response: "Veuillez entrer un mot de passe (ex. : JESUISMEMBRE66 pour Admin, JESUISTRESORIER444 pour Trésorier, PRESIDENT000 pour Président, SECRETAIRE000 pour Secrétaire)." },
+  { keyword: "22222222", response: "Veuillez entrer un mot de passe (ex. : JESUISMEMBRE66 pour Admin, JESUISTRESORIER444 pour Trésorier, PRESIDENT000 pour Président, SECRETAIRE000 pour Secrétaire)." }
 ];
 
 function getChatbotResponse(message) {
